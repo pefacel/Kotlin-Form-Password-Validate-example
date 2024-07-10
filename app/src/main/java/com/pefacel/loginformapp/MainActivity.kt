50e8191be408fcd0efcbe727ceda8c3d0bdac479
Pedro <pedro@192.168.1.7>
2024-07-09 20:52:46 -0400

    feature2

--- app/src/main/java/com/pefacel/loginformapp/MainActivity.kt
+++ app/src/main/java/com/pefacel/loginformapp/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.pefacel.loginformapp.databinding.ActivityMainBinding
 
+// Feature 2
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
